d4d8ea3ac6bad7e90ab7fdf8d25e837f0e684db9
Saurabh <saurabhp14344@gmail.com>
2019-06-08 01:57:58 +0530

    scraping and save data in jason and csv

--- scrapy_project/spiders/goodreads.py
+++ scrapy_project/spiders/goodreads.py
@@ -7,25 +7,21 @@ class GoodReads(scrapy.Spider):
     #request
 
     def start_requests(self):
-        urls = [
-            'https://www.goodreads.com/quotes?page=1',
-            'https://www.goodreads.com/quotes?page=2',
-            'https://www.goodreads.com/quotes?page=3',
-            'https://www.goodreads.com/quotes?page=4',
-            'https://www.goodreads.com/quotes?page=5',
-            'https://www.goodreads.com/quotes?page=6',
-            'https://www.goodreads.com/quotes?page=7',
-            'https://www.goodreads.com/quotes?page=8',
-            'https://www.goodreads.com/quotes?page=9',
-            'https://www.goodreads.com/quotes?page=10',
-        ]
-        for url in urls:
-            yield scrapy.Request(url=url, callback=self.parse)
+        url = 'https://www.goodreads.com/quotes?page=1'
+
+        yield scrapy.Request(url=url, callback=self.parse)
 
     #respone
 
     def parse(self, response):
-        page_number = response.url.split("=")[1]
-        _file = "{0}.html".format(page_number)
-        with open(_file, 'wb') as f:
-            f.write(response.body)
+        for quote in response.selector.xpath("//div[@class='quote']"):
+            yield {
+                'text': quote.xpath(".//div[@class='quoteText']/text()").extract_first(),
+                'author': quote.xpath(".//span[@class='authorOrTitle']/text()").extract_first(),
+                'tags': quote.xpath(".//div[@class='greyText smallText left']/a/text()").extract()
+            }
+        next_page = response.selector.xpath("//a[@class='next_page']/@href").extract_first()
+
+        if next_page is not None:
+            next_page_link = response.urljoin(next_page)
+            yield scrapy.Request(url=next_page_link, callback=self.parse)
